e1cf7e2753575802402cd0883dd5d20cb9aad5dc
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-03-24 11:41:53 -0300

    Feat: Ajustado rota GET /crops para utilizar datas

--- src/main/java/com/betrybe/agrix/controllers/CropController.java
+++ src/main/java/com/betrybe/agrix/controllers/CropController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
 * CropController.
 */
+@RestController
+@RequestMapping()
 public class CropController {
   private final CropService cropService;
     
